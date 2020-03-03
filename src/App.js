310332f6906b3c71af02237145b05e50eb4042ab
Muhammad Reza V <36061108+mrezav@users.noreply.github.com>
2020-03-03 07:18:45 +0700

    Back To Fresh
    Remove Route Reactjs

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch, Link, useParams} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-  <BrowserRouter>
-    <div>
-      <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/users'>User</Link></li>
-        </ul>
-      </nav>
-
-      <main>
-        <Route path='/'><Home /></Route>
-        <Route path='/users'><Users /></Route>
-        <Route path='/detail/:name'><DetailUser /></Route>
-      </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React {React.version}
+        </a>
+      </header>
     </div>
-  </BrowserRouter>
   );
-}
-
-function Home(){
-  return (  
-    <h2>Home Page</h2>
-  )
-}
-
-function Users(){
-  return (
-    <div>
-      <h2>User Page</h2>
-      <Link to='/detail/:Muhammad'>Muhammad</Link><br></br>
-      <Link to='/detail/:reza'>Reza</Link><br></br>
-      <Link to='/detail/:vahlevi'>Vahlevi</Link>
-    </div>
-    )
-}
-
-function DetailUser(){
-  let {name} = useParams();
-
-  return <h2>Halaman {name} </h2>
 }
 
 export default App;
